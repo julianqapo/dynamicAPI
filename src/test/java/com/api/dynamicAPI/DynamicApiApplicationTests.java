9aa5886733fa64c8f92e13f06e9b9e8e96584b77
Julian <Julian.Qapo@gmail.com>
2026-06-09 11:34:11 -0400

    first commit

---
+++ src/test/java/com/api/dynamicAPI/DynamicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.dynamicAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
